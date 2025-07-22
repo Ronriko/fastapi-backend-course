f2639ff3f53e04ed9ac58ba29308534b7a020803
Ronriko <r0nrik0@yandex.ru>
2025-07-22 22:00:06 +0300

    version before review

--- simple_backend/src/task_tracker/main.py
+++ simple_backend/src/task_tracker/main.py
@@ -1,19 +1,37 @@
 from fastapi import FastAPI
+from pydantic import BaseModel
 
 app = FastAPI()
+tasks = []
+
+class Task(BaseModel):
+    id: int
+    title:str
+    status: str
 
 @app.get("/tasks")
 def get_tasks():
-    pass
+    return tasks
 
 @app.post("/tasks")
 def create_task(task):
-    pass
+    tasks.append(task)
+    return task
 
 @app.put("/tasks/{task_id}")
-def update_task(task_id: int):
-    pass
+def update_task(task_id: int, update_task: Task):
+    for i, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks[i] = update_task
+            return update_task
+    return {"error": "Task not found"}
+        
 
 @app.delete("/tasks/{task_id}")
 def delete_task(task_id: int):
-    pass
+    for i, task in enumerate(tasks):
+        if task.id == task_id:
+            tasks.pop(i)
+            return {"message": "Task deleted"}
+    return {"error": "Task not found"}
+
